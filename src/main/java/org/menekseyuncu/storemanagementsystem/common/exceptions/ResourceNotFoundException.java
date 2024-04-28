ba5476965cce4ab3fb9ad1943c065d61d3e4a6ff
MenekseYuncu <baioretto66@gmail.com>
2024-04-28 19:39:31 +0300

    ResourceNotFoundException added

--- src/main/java/org/menekseyuncu/storemanagementsystem/common/exceptions/ResourceNotFoundException.java
+++ src/main/java/org/menekseyuncu/storemanagementsystem/common/exceptions/ResourceNotFoundException.java
@@ -3,10 +3,15 @@ package org.menekseyuncu.storemanagementsystem.common.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException(String message) {
-        super(message);
+    @Serial
+    private static final long serialVersionUID = 1721232167659910474L;
+
+    public ResourceNotFoundException() {
+        super("Resource not found!!");
     }
 }

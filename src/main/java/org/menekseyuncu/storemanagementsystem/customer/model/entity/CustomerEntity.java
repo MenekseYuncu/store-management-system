87af86012ff73dd901fd26313fc01c72a4b91099
MenekseYuncu <baioretto66@gmail.com>
2024-04-29 23:58:41 +0300

    pomxml and customer entity modified

--- src/main/java/org/menekseyuncu/storemanagementsystem/customer/model/entity/CustomerEntity.java
+++ src/main/java/org/menekseyuncu/storemanagementsystem/customer/model/entity/CustomerEntity.java
@@ -1,16 +1,22 @@
 package org.menekseyuncu.storemanagementsystem.customer.model.entity;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.menekseyuncu.storemanagementsystem.cart.model.entity.CartEntity;
 import org.menekseyuncu.storemanagementsystem.common.model.BaseEntity;
+import org.menekseyuncu.storemanagementsystem.orders.model.entity.OrderEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,11 +24,6 @@ import org.menekseyuncu.storemanagementsystem.common.model.BaseEntity;
 @NoArgsConstructor
 @Table(name = "sm_customer")
 public class CustomerEntity extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
 
     @Column(name = "name")
     private String name;
@@ -32,4 +33,11 @@ public class CustomerEntity extends BaseEntity {
 
     @Column(name = "address")
     private String address;
+
+    @OneToOne(mappedBy = "customer", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private CartEntity cart;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<OrderEntity> orders = new ArrayList<>();
+
 }
